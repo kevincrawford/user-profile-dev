ed01894d278d79ce1cf30742726ab11a5bcd8894
gitlab@evertheme.com <gitlab@evertheme.com>
2020-01-07 10:22:47 -0600

    fix typo in scholarship

--- client/src/app/feature/scholarships/ScholarshipTeacher.jsx
+++ client/src/app/feature/scholarships/ScholarshipTeacher.jsx
@@ -13,11 +13,7 @@ const actions = {
 
 export class ScholarshipTeacher extends Component {
   handleSocialClick = type => {
-    const url =
-      window.location.protocol +
-      '//' +
-      window.location.host +
-      '/scholarships/teacher';
+    const url = window.location.protocol + '//' + window.location.host + '/scholarships/teacher';
     if (type === 'twitter') {
       window.open(
         `https://twitter.com/intent/tweet?text=SPEDxchange:%202020%20Special%20Education%20Teacher%20Scholarship%20$1,000&amp;url=${url}`,
@@ -33,20 +29,12 @@ export class ScholarshipTeacher extends Component {
       );
     }
     if (type === 'facebook') {
-      window.open(
-        `https://www.facebook.com/sharer/sharer.php?u=${url}`,
-        'facebook-share-dialog',
-        'width=600,height=480'
-      );
+      window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`, 'facebook-share-dialog', 'width=600,height=480');
     }
   };
 
   setEmailLink = () => {
-    const url =
-      window.location.protocol +
-      '//' +
-      window.location.host +
-      '/scholarships/teacher';
+    const url = window.location.protocol + '//' + window.location.host + '/scholarships/teacher';
     return `mailto:?subject=2020%20Special%20Education%20Teacher%20Scholarship%20$1,000&body=I thought you might be interested in reading this SPED Talk article.%0D%0A%0D%0A2020%20Special%20Education%20Teacher%20Scholarship%20$1,000%0D%0A${url}%0D%0A%0D%0A`.replace(
       / /g,
       '%20'
@@ -74,45 +62,20 @@ export class ScholarshipTeacher extends Component {
               2020 Special Education Teacher Scholarship - <small>$</small>1,000
             </h1>
             <div>
-              <img
-                src='/assets/img/scholarship.png'
-                alt='Special Education Teacher Scholarship'
-              />
+              <img src='/assets/img/scholarship.png' alt='Special Education Teacher Scholarship' />
             </div>
           </div>
           <div className='article-figure'>
             <div className='share'>SHARE</div>
-            <Icon
-              link
-              circular
-              name='twitter'
-              onClick={() => this.handleSocialClick('twitter')}
-            />
-            <Icon
-              link
-              circular
-              name='linkedin'
-              onClick={() => this.handleSocialClick('linkedin')}
-            />
-            <Icon
-              link
-              circular
-              name='facebook'
-              onClick={() => this.handleSocialClick('facebook')}
-            />
+            <Icon link circular name='twitter' onClick={() => this.handleSocialClick('twitter')} />
+            <Icon link circular name='linkedin' onClick={() => this.handleSocialClick('linkedin')} />
+            <Icon link circular name='facebook' onClick={() => this.handleSocialClick('facebook')} />
             <a href={this.setEmailLink()}>
               <Icon link circular name='envelope outline' />
             </a>
           </div>
-          <p>
-            SPEDxchange is offering a $1,000 scholarship for undergraduate or
-            graduate students that are interested in working with special needs
-            students.
-          </p>
-          <p>
-            This award is for undergraduate or graduate students that are
-            pursuing a degree in folowing special education or a related fields:
-          </p>
+          <p>SPEDxchange is offering a $1,000 scholarship for undergraduate or graduate students that are interested in working with special needs students.</p>
+          <p>This award is for undergraduate or graduate students that are pursuing a degree in following special education or a related fields:</p>
           <p>
             <strong>Special Education Teacher</strong>
             <br />
