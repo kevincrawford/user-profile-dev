f2cac501eddba3d60762c70cc526ef911e38f7b6
spedxchange <content@spedxchange.com>
2020-02-13 22:24:47 -0600

    fix console.logs

--- client/src/app/common/ui/auth/AuthWrapper.jsx
+++ client/src/app/common/ui/auth/AuthWrapper.jsx
@@ -10,11 +10,9 @@ export const UserIsAuthenticated = connectedReduxRedirect({
   allowRedirectBack: true,
   redirectPath: '/ask',
   authenticatedSelector: state => {
-    console.log('UserIsAuthenticated: state: ', state);
     return state.auth.loading ? true : state.auth.authenticated;
   },
   redirectAction: newLoc => dispatch => {
-    console.log('UserIsAuthenticated: newLoc: ', newLoc);
     dispatch(openModal('UnauthModal'));
   }
 });
@@ -24,12 +22,9 @@ export const UserIsAdmin = connectedReduxRedirect({
   allowRedirectBack: true,
   redirectPath: '/news',
   authenticatedSelector: state => {
-    console.log('UserIsAdmin: state: ', state);
     return state.auth.loading ? true : state.auth.authenticated && state.auth.currentUser && state.auth.currentUser.roles.findIndex(isAdmin) > -1;
   },
   redirectAction: newLoc => dispatch => {
-    console.log('UserIsAdmin: newLoc: ', newLoc);
-    console.log('UserIsAdmin: dispatch: ', dispatch);
     dispatch(openModal('UnauthModal'));
   }
 });
